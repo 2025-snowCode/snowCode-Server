4d135f7e5acc131172e4b186240da103f57cd3e1
ajeong <ajung7038@naver.com>
2025-09-14 18:07:54 +0900

    Feat: 1차 Swagger 기입 #39

--- src/main/java/snowcode/snowcode/common/config/SwaggerConfig.java
+++ src/main/java/snowcode/snowcode/common/config/SwaggerConfig.java
@@ -1,8 +1,11 @@
 package snowcode.snowcode.common.config;
 
-import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.Schema;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,5 +24,34 @@ public class SwaggerConfig {
                 .title("snowCode Server API")
                 .description("swagger description")
                 .version("1.0.0");
+    }
+
+
+    @Bean
+    public OperationCustomizer operationCustomizer() {
+        return (operation, handlerMethod) -> {
+            this.addSchema(operation);
+            return operation;
+        };
+    }
+
+    private void addSchema(Operation operation) {
+        final Content content = operation.getResponses().get("200").getContent();
+        if (content != null) {
+            content.forEach((mediaTypeKey, mediaType) -> {
+                Schema<?> originalSchema = mediaType.getSchema();
+                Schema<?> wrappedSchema = wrapSchema(originalSchema);
+                mediaType.setSchema(wrappedSchema);
+            });
+        }
+    }
+
+    private Schema<?> wrapSchema(Schema<?> originalSchema) {
+        final Schema<?> wrapperSchema = new Schema<>();
+
+        wrapperSchema.addProperty("success", new Schema<>().type("boolean").example(true));
+        wrapperSchema.addProperty("responses", originalSchema);
+
+        return wrapperSchema;
     }
 }
