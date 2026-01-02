29ffe65f195f963aed0cc0cf041699cb5b40e665
ajeong <ajung7038@naver.com>
2026-01-02 09:49:50 +0900

    Del: 내 정보 조회 API 제거 #71

--- src/main/java/snowcode/snowcode/auth/controller/MemberController.java
+++ src/main/java/snowcode/snowcode/auth/controller/MemberController.java
@@ -28,19 +28,19 @@ public class MemberController {
     private final MemberService memberService;
     private final AuthService authService;
 
-    @GetMapping("/me")
-    @Operation(summary = "내 정보 조회 API", description = "내 정보 조회 (이름)")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "내 정보 조회 성공",
-                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = MyProfileResponse.class))}),
-            @ApiResponse(responseCode = "404", description = "회원을 찾을 수 없습니다",
-                    content = {@Content(schema = @Schema(implementation = BasicResponse.class))}),
-    })
-    public BasicResponse<MyProfileResponse> findMember() {
-        Long memberId = authService.loadMember().getId();
-        MyProfileResponse myProfileResponse = memberService.findMemberById(memberId);
-        return ResponseUtil.success(myProfileResponse);
-    }
+//    @GetMapping("/me")
+//    @Operation(summary = "내 정보 조회 API", description = "내 정보 조회 (이름)")
+//    @ApiResponses(value = {
+//            @ApiResponse(responseCode = "200", description = "내 정보 조회 성공",
+//                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = MyProfileResponse.class))}),
+//            @ApiResponse(responseCode = "404", description = "회원을 찾을 수 없습니다",
+//                    content = {@Content(schema = @Schema(implementation = BasicResponse.class))}),
+//    })
+//    public BasicResponse<MyProfileResponse> findMember() {
+//        Long memberId = authService.loadMember().getId();
+//        MyProfileResponse myProfileResponse = memberService.findMemberById(memberId);
+//        return ResponseUtil.success(myProfileResponse);
+//    }
 
     @GetMapping
     @Operation(summary = "회원 전체 조회", description = "회원 전체 조회 API")
