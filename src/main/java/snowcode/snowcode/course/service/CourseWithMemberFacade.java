ee8717d99baf4c30e71d5f52d310e7581b7e745e
ajeong <ajung7038@naver.com>
2026-03-26 20:09:26 +0900

    Feat: chatRoomId 추가 #147

--- src/main/java/snowcode/snowcode/course/service/CourseWithMemberFacade.java
+++ src/main/java/snowcode/snowcode/course/service/CourseWithMemberFacade.java
@@ -4,6 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import snowcode.snowcode.auth.service.MemberService;
+import snowcode.snowcode.chatRoom.exception.ChatRoomErrorCode;
+import snowcode.snowcode.chatRoom.exception.ChatRoomException;
+import snowcode.snowcode.chatRoom.service.ChatRoomService;
 import snowcode.snowcode.course.domain.Course;
 import snowcode.snowcode.course.dto.CourseDetailAdminResponse;
 import snowcode.snowcode.course.dto.CourseDetailStudentResponse;
@@ -24,18 +27,27 @@ public class CourseWithMemberFacade {
     private final UnitService unitService;
     private final UnitWithAssignmentFacade unitWithAssignmentFacade;
     private final MemberService memberService;
+    private final ChatRoomService chatRoomService;
 
     public CourseDetailStudentResponse createStudentCourseResponse(Long memberId, Long courseId) {
         Course course = courseService.findCourse(courseId);
         List<Unit> unitList = unitService.findAllByCourseId(courseId);
-
+        
+        // 단원 및 과제 찾기
         List<UnitDetailStudentResponse> unitDtoList = new ArrayList<>();
 
         for (Unit unit : unitList) {
             unitDtoList.add(unitWithAssignmentFacade.createStudentUnitResponse(memberId, unit.getId()));
         }
 
-        return CourseDetailStudentResponse.of(course, unitDtoList);
+        // admin 찾기
+        Long adminId = course.getCreatedBy();
+
+        // chatRoomId 조회
+        Long chatRoomId = chatRoomService.findChatRoomByMembers(adminId, memberId)
+                .orElseThrow(() -> new ChatRoomException(ChatRoomErrorCode.NOT_FOUND_CHAT_ROOM)).getId();
+
+        return CourseDetailStudentResponse.of(course, chatRoomId, unitDtoList);
     }
 
     public CourseDetailAdminResponse createAdminCourseResponse(Long courseId) {
